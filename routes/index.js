104b81c0e97fb78367c48974436634916f9852d8
Daniel Partika <72474345+DanPartika@users.noreply.github.com>
2022-11-23 22:25:39 -0500

    small changes

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
 //Here you will require both route files and export the constructor method as shown in lecture code where there is more than one route file. Look at lecture 6 lecture code for example
 
 // when the route is /Apartments use the routes defined in Apartments.js routing file, when the route is /reviews use the routes defined in reviews.js routing file, all other enpoints should return a 404 as shown in the lecture code.
-const ApartmentsRoutes = require("./Apartments");
+const apartmentsRoutes = require("./apartments");
 const reviewsRoutes = require("./reviews");
 
 const constructorMethod = (app) => {
-  app.use("/Apartments", ApartmentsRoutes);
+  app.use("/apartments", apartmentsRoutes);
   app.use("/reviews", reviewsRoutes);
 
   app.use("*", (req, res) => {
