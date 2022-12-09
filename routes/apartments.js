9f0c2f6c61837ad238d0d6bdd8fa83ed2bc99192
CodyBF <codynb12@gmail.com>
2022-12-09 15:03:09 -0500

    starting to add apt to website

--- routes/apartments.js
+++ routes/apartments.js
@@ -7,7 +7,7 @@ const apartmentsData = data.Apartments;
 const usersData = data.users
 const { ObjectId } = require("mongodb");
 const helpers = require("../helpers");
-const { getApartmentById } = require("../data/apartments");
+const { getApartmentById, createApartment } = require("../data/apartments");
 const path = require('path');
 
 // router.route("/") //homepage
@@ -43,7 +43,7 @@ router
 
 
 router
-  .route("/apartments/:ApartmentId") //singular apt
+  .route("/apartments/apartment/:ApartmentId") //singular apt
   .get(async (req, res) => {
     //code here for GET
     if (req.session.user) {
@@ -53,11 +53,12 @@ router
         //let apt = {};
         try{
           const apt = await getApartmentById(ApartmentId); //!idk if this saves outside the try catch
+          const tempData = {title: title, apt:apt};
+          return res.render("apartments/apartment",tempData);
         } catch (e) {
           return res.status(400).render("error", {title: "Apartment Not Found", message: "400 Error: Apartment not found."});  // can alert this instead
         }
-        const tempData = {title: title, apt:apt};
-        return res.render("apartment",tempData);
+        
       } catch (e) {
         
         return res.status(404).render("error", {title: "Apartment Not Found", message: "404 Error: Page not found."});
@@ -79,6 +80,36 @@ router
   .post(async (req,res) => {
     if (req.session.user) {
       // return res.render('apartments/addApt');
+      try{
+        let apartmentData = req.body;
+        let apartmentName = apartmentData.apartmentNameInput; 
+        let streetAddress = apartmentData.streetAddressInput;
+        let rentPerMonth = apartmentData.rentPerMonthInput;
+        let rentDuration = apartmentData.rentDurationInput;
+        let maxResidents = apartmentData.maxResidentsInput;
+        let numBedrooms = apartmentData.numBedroomsInput;
+        let numBathrooms = apartmentData.numBathroomsInput;
+        let laundry = apartmentData.laundryInput;
+        if(laundry == "true") laundry = true
+        else laundry = false
+        let floorNum = apartmentData.floorNumInput;
+        let roomNum = apartmentData.roomNumInput;
+        let appliancesIncluded = apartmentData.appliancesIncludedInput;
+        appliancesIncluded = appliancesIncluded.split(",");
+        let maxPets = apartmentData.maxPetsInput;
+        let utilitiesIncluded = apartmentData.utilitiesIncludedInput;
+        utilitiesIncluded = utilitiesIncluded.split(",")
+        //console.log(laundry)
+      
+        let apt = createApartment(apartmentName, streetAddress, rentPerMonth, rentDuration, maxResidents, numBedrooms, numBathrooms, laundry, floorNum, roomNum, appliancesIncluded, maxPets, utilitiesIncluded);
+        if(!apt.overallRating == 0) return res.render('error',{title:"Error in creating apartment"});
+        let pathRedirect = "/apartments/apartment/:"+apt._id;
+        res.redirect(pathRedirect);
+      } catch (e) {
+        return res.render('error',{title:"Error in creating apartment", message:e});
+      }
+
+
     } else {
       return res.render('userAccount/login',{});
     }
