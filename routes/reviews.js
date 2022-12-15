4fa6258555fd4a0233cf2980c78a21f6bc8ae383
Elton Vaz <eltonevaz@gmail.com>
2022-12-14 20:04:06 -0500

    delete reviews

--- routes/reviews.js
+++ routes/reviews.js
@@ -119,18 +119,18 @@ const { review } = require("../data");
   })
 
 ///reviews/deleteReview/
-/*
+
   router
     .route("/deleteReview/:reviewId")
-    .get(async (req,res) => {
+    // .get(async (req,res) => {
 
-      if (req.session.user) {
-        return res.redirect('userAccount/userhomepage',{user:req.session.user});
-      } else {
-        return res.render('userAccount/login',{user:req.session.user});
-      }
-    })
-    .delete(async (req,res) => {
+    //   if (req.session.user) {
+    //     return res.render('userAccount/userhomepage',{user:req.session.user});
+    //   } else {
+    //     return res.render('userAccount/login',{user:req.session.user});
+    //   }
+    // })
+    .post(async (req,res) => {
       req.params.reviewId = req.params.reviewId.trim();
 
       if (!ObjectId.isValid(req.params.reviewId)) {
@@ -139,11 +139,13 @@ const { review } = require("../data");
       }
       try {
         const review = await removeReview(req.params.reviewId);
-        return res.redirect('userAccount/userhomepage',{user:req.session.user});
+        console.log(req.params.reviewId);
+        console.log("1")
+        return res.render('userAccount/userhomepage',{user:req.session.user});
       } catch (e) {
         res.render('error', {title: "Error", message: e});
       }
-    }) */
+    })
 // router
 //   .route("/review/:reviewId")
 //   .get(async (req, res) => {
