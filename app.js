e41e242f0794cafd3d3efc291d898e70b5bbacfa
Hari20-02 <sriharishankar@outlook.sg>
2022-12-15 21:00:52 -0500

    rename upload

--- app.js
+++ app.js
@@ -5,6 +5,25 @@ const configRoutes = require('./routes');
 const exphbs = require('express-handlebars');
 const session = require('express-session');
 const multer = require("multer");
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  }
+})
+var upload = multer({ storage: storage })
+
+app.use(express.static(__dirname + '/views/apartments'));
+app.use('/uploads', express.static('uploads'));
+
+app.post('/profile-upload-single', upload.single('profile-file'))
+
+
+
+
 const static = express.static(__dirname + '/public');
 
 
@@ -42,33 +61,10 @@ app.use(async (req, res, next) => {
   next()
 })
 
-var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, './uploads')
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname)
-  }
-})
-var upload = multer({ storage: storage })
- app.use(express.static(__dirname + '/views/apartments'));
-const static1 = express.static(__dirname + "/uploads");
-app.use("/uploads", static1);
-
-app.post('/profile-upload-single', upload.single('profile-file'), function (req, res, next) {
-  // req.file is the `profile-file` file
-  // req.body will hold the text fields, if there were any
-  console.log(JSON.stringify(req.file))
-  var response = '<a href="/">Home</a><br>'
-  response += "Files uploaded successfully.<br>"
-  response += `<img src="${req.file.path}" /><br>`
-  return res.send(response)
-})
-
-
 
 
 configRoutes(app);
+
 
 app.listen(3000, () => {
   console.log("We've now got a server!");
